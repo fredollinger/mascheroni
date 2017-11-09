86ce02a5caca304b138dc4dbd3b899160bed91a9
Frederick Ollinger <follinge@gmail.com>
2017-11-09 07:24:33 -0800

    Document write_num()

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,17 @@ use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::path::Path;
 
+/// Write a number to a given filename
+///
+/// If the file all ready exists, the number will be appended.
+///
+/// Each number gets its own line and is comma delimited so one can
+/// diff the output of two files more easily as well as import the data
+/// into other application.
+///
+/// Example:
+///
+/// write_num(1, "foo.txt");
 pub fn write_num(num: u64, fname: &str) -> std::io::Result<()>
 {
     let s: String = num.to_string() + ",\n";
