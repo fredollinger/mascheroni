acfde3739bc7301894fc3a11c9461d26c438d5d3
Frederick Ollinger <follinge@gmail.com>
2017-11-09 07:00:59 -0800

    Add write_num()
    We can now write a number to a file.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::path::Path;
+
+pub fn write_num(num: u64, fname: &str) -> std::io::Result<()>
+{
+    let s: String = num.to_string() + ",\n";
+    if Path::new(fname).exists() {
+        let mut file = OpenOptions::new().append(true).open(fname)?;
+        file.write_all(s.as_bytes())?;
+    }
+    else {
+        let mut file = File::create(fname)?;
+        file.write_all(s.as_bytes())?;
+    }
+    return Ok(());
+}
 
 pub fn public_function() {
     println!("called rary's `public_function()`");
